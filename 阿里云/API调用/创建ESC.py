3de15bc82b26a43284b12fbfadb5dca07ea1c1d5
RookieToExpert <rui0821722@gmail.com>
2025-03-01 03:45:41 -0500

    Create 创建ESC.py

---
+++ 阿里云/API调用/创建ESC.py
@@ -0,0 +1,5 @@
+from aliyunsdkcore.client import AcsClient
+client = AcsClient("<access_key_id>", "<access_secret>", "cn-hangzhou")
+request = CreateInstanceRequest()
+request.set_ImageId("centos_7")
+response = client.do_action_with_exception(request)
